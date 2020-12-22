38555189d12f684d020e5ad5b906fb9c07dc4afa
Jordan <jordanleelong5@gmail.com>
2020-12-22 13:43:43 -0800

    refactored component to get data depending on what zipcode was added in

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -4,26 +4,31 @@ import React, {useEffect, useState} from 'react'
 export const CurrentWeather = () => {
 
     let [currentData, setCurrentData] = useState([]); 
+    let [input, setInput] = useState(''); 
     
     const {REACT_APP_WEATHER_KEY} = process.env
 
-    useEffect(() => {
-        fetch(`http://api.weatherapi.com/v1/current.json?key=${REACT_APP_WEATHER_KEY}&q=94518`)
-        .then(res => res.json())
-        .then(res => setCurrentData(res))
-    }, [REACT_APP_WEATHER_KEY])
-
+  
+    const onTermSubmit = async (event) => {
+        event.preventDefault()
+           const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${REACT_APP_WEATHER_KEY}&q=${input}`)
+         const data = await response.json()
+         setCurrentData(data)
+         setInput('')
+    }
 
     const mapCurrentLocationData = () => {
         console.log(currentData)
     }
 
-    const mapCurrentData = () => {
-        
-    }
 
 return (
     <div>
+        <div>
+            <form onSubmit={onTermSubmit}>
+            <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Search for a location by zip code' />
+            </form>
+        </div>
        {mapCurrentLocationData()}
     </div>
 )
