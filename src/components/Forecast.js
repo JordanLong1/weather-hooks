d444b6a547724608389eabbdd4181383d2a55fe3
Jordan <jordanleelong5@gmail.com>
2020-12-23 23:42:02 -0800

    can successfully view forecast data in child

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -4,7 +4,7 @@ import React, {useState} from 'react'
 
 export const Forecast = (props) => {
 
-    console.log(props)
+    console.log('made it down', props)
     return (
         <div> 
             {/* {getForecast()} */}
