e66a7545fb7d880ec4fcc3bf8a2cf1f3733b6feb
Jordan <jordanleelong5@gmail.com>
2020-12-25 23:57:06 -0800

    created forecast map, going to put each day into cards

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -5,7 +5,10 @@ import React, {useState} from 'react'
 export const Forecast = (props) => {
 
     const mapForecastPropsToList = (data) => {
-        console.log('hello', data)
+        // console.log('hello', data.forecast.forecastda)
+        let forecastMap = data.forecast.forecastday.map(day => {
+            return console.log(day)
+        })
     }
 
     return (
