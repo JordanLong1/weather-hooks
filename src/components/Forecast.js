3a95a80c9a63b334f10ef436c4a34c3bbf49ec87
Jordan <jordanleelong5@gmail.com>
2020-12-28 11:15:45 -0800

    added min & max temps and added flex direction of row to display side by side

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -5,13 +5,12 @@ import React, {useState} from 'react'
 export const Forecast = (props) => {
 
     const mapForecastPropsToList = (data) => {
-      
-            return data.forecast.forecastday.map(day => {
+        return data.forecast.forecastday.map(day => {
             return (
                 <div className='forecast-details'>
                     <img src={day.day.condition.icon} alt='' />
                     <h4>{day.day.condition.text}</h4>
-                    
+                    <li key={day.date}>High: {day.day.maxtemp_f}, Low: {day.day.mintemp_f}</li>
                 </div>
             )
         })
